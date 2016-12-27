dc9d85c5d5022e7e49ed26a0dd91b7d8e6a88716
BBKIM <jotun9935@gmai.com>
2016-12-27 20:46:07 +0900

    edit : CUI

--- simpleBenchmark.py
+++ simpleBenchmark.py
@@ -16,16 +16,18 @@ def executeTarget(target, count):
 
 
 def makeGraph(timeList):
+    print("Makeing Graph Start")
     x = range(len(timeList))
     y = timeList
     plt.plot(x,y)
     #plt.show()
-
+    print("Makeing Graph End")
 
 def saveGraphImage(outFile):
+    print("Saving GraphImage Start")
     plt.savefig(outFile)
     plt.close()  #if close plt
-
+    print("Saving GraphImage END")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -36,10 +38,13 @@ if __name__ == "__main__":
     timeList = []
 
     for num in range(measureNumber):
+        print("\n%d / %d :%d\n" %(num+1, measureNumber ,num+1))
         totTime = executeTarget(target = sys.argv[1], count = int(sys.argv[2]))
         timeList.append(totTime)
+
     print("")
 
+    print("sorting")
     timeList.sort()
     makeGraph(timeList)
     saveGraphImage(outFile)
