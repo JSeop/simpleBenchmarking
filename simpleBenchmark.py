3d5b4c146f7a08f3e6d0cb08d156cf53dff57053
BBKIM <jotun9935@gmai.com>
2016-12-27 20:05:40 +0900

    add : save result to PNG file

--- simpleBenchmark.py
+++ simpleBenchmark.py
@@ -10,38 +10,38 @@ def executeTarget(target, count):
         subprocess.call(sys.argv[1])
         endTime = time.time()
         totTime += endTime - startTime
-        #print(".")
         sys.stdout.write('.')
+
     return totTime
+
 
 def makeGraph(timeList):
     x = range(len(timeList))
     y = timeList
     plt.plot(x,y)
-    plt.show()
+    #plt.show()
 
 
+def saveGraphImage(outFile):
+    plt.savefig(outFile)
+    plt.close()  #if close plt
 
-
-def saveGraphImage(img):
-    print("")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage ( " + sys.argv[0] + " ) : <target program> <count>")
         sys.exit()
     measureNumber = 50 #To do> config file
+    outFile = "output_file"
     timeList = []
 
     for num in range(measureNumber):
         totTime = executeTarget(target = sys.argv[1], count = int(sys.argv[2]))
         timeList.append(totTime)
-        #print("")
-        #print("="*30 + "Now %d 'th loop" %(num+1) + "="*30 )
+    print("")
 
     timeList.sort()
-    img = makeGraph(timeList)
-    saveGraphImage(img)
-    #print(timeList)
-    #print(len(timeList))
+    makeGraph(timeList)
+    saveGraphImage(outFile)
+
     
