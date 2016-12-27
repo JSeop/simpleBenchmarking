026af27758b1cf6aaec58615a645319f34cbc09c
BBKIM <jotun9935@gmai.com>
2016-12-27 14:25:01 +0900

    initial

---
+++ simpleBenchmark.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import time
+
+def executeTarget(target, count):
+    totTime = 0
+    for i in range(count):
+        startTime = time.time()
+        subprocess.call(sys.argv[1])
+        endTime = time.time()
+        totTime += endTime - startTime
+        #print(".")
+        sys.stdout.write('.')
+    return totTime
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage ( " + sys.argv[0] + " ) : <target program> <count>")
+        sys.exit()
+    measureNumber = 50 #To do> config file
+    timeList = []
+
+    for num in range(measureNumber):
+        totTime = executeTarget(target = sys.argv[1], count = int(sys.argv[2]))
+        timeList.append(totTime)
+        print("")
+        print("="*30 + "Now %d 'th loop" %(num+1) + "="*30 )
+
+    timeList.sort()
+    print(timeList)
+    #print(len(timeList))
+    
